83a3761169953aa695bf173e1d340ca7b079a1c4
qpdian <qpdiam@gmail.com>
2019-02-26 18:28:54 -0500

    feat: call api server in signup page

---
+++ src/pages/sign-up.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+
+import SEO from "../components/seo"
+import { Box, Button, Form, FormField, Heading } from "grommet"
+import { FormClock } from "grommet-icons"
+import { navigate } from "gatsby"
+
+const axios = require("axios")
+
+axios.defaults.baseURL = "http://localhost:3000"
+
+const SignupPage = () => (
+  <Box height="100vh" width="100vw" align="center" justify="center" pad="small">
+    <SEO title="Login" keywords={[`register`, `quekiwi`, `libros`]} />
+    <Heading level={5}>Regístrate en Quekiwi</Heading>
+    <Box width="medium">
+      <RegisterForm />
+      <Suggestion />
+    </Box>
+  </Box>
+)
+
+const RegisterForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const submit = ({ value }) => {
+    setIsSubmitting(true)
+    axios
+      .post("/auth/signup", value)
+      .then(response => {
+        window.localStorage.setItem("token", response.data.token)
+        setIsSubmitting(false)
+        navigate("/main")
+      })
+      .catch(error => {
+        if (error.response) {
+          const dataError = error.response.data
+          alert(dataError.message)
+        }
+        setIsSubmitting(false)
+      })
+  }
+  const signupData = { name: "", email: "", repeatedEmail: "" }
+
+  return (
+    <Form onSubmit={submit} value={signupData}>
+      <FormField label="Correo" name="email" required type="email" />
+      <FormField
+        name="repeatedEmail"
+        required
+        label="Escriba de nuevo su correo"
+        type="email"
+        validate={(repeatedEmail, form) =>
+          form.email !== repeatedEmail && "Sus correos no coinciden"
+        }
+      />
+      <FormField label="Contraseña" required name="password" type="password" />
+      <Box direction="row" justify="center" margin={{ top: "medium" }}>
+        {isSubmitting ? (
+          <FormClock />
+        ) : (
+          <Button type="submit" fill label="Registrarme" primary />
+        )}
+      </Box>
+    </Form>
+  )
+}
+
+const Suggestion = () => (
+  <p>
+    <Link to="/login">¿Ya tengo una cuenta?</Link> Voy a loguearme{" "}
+    <span role="img" aria-label="feliz">
+      🙂
+    </span>
+  </p>
+)
+
+export default SignupPage
