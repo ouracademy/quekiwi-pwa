4e0505c629ba2a2730acd200fb0a91955c58cecf
qpdian <qpdiam@gmail.com>
2019-02-28 18:04:09 -0500

    feat: show book fields in search result

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,24 +1,31 @@
 import React, { useState } from "react"
-import { Box, TextInput, Heading } from "grommet"
+import { Box, TextInput, Heading, Anchor } from "grommet"
 import { Search } from "grommet-icons"
 import SEO from "./seo"
 
 const SearchResult = ({ books }) => {
-  const bookList = books.map((book, index) => (
-    <Box
-      pad="medium"
-      direction="column"
-      width="large"
-      height="small"
-      elevation="small"
-    >
-      <Heading color="neutral-1" level={5}>
-        {book.title}
-      </Heading>
-      <Heading color="neutral-1" level={6}>
-        {book.author}
-      </Heading>
-    </Box>
+  const bookList = books.map(({ id, title, provider, price, unit }, index) => (
+    <Anchor href={`book/${id}`} key={index}>
+      {" "}
+      <Box
+        pad="medium"
+        direction="column"
+        width="large"
+        height="small"
+        elevation="small"
+      >
+        <Heading color="neutral-1" level={5}>
+          {title}
+        </Heading>
+        <Box direction="row">
+          <span> {unit}</span>
+          <Heading color="neutral-1" level={2}>
+            {price}
+          </Heading>
+        </Box>
+        <Box round="medium">{provider}</Box>
+      </Box>
+    </Anchor>
   ))
   return (
     <Box
@@ -61,17 +68,21 @@ const SearchInput = ({ search }) => (
 export const DashBoard = () => {
   const allBooks = [
     {
+      id: 1,
       author: "Must Read Summaries",
       title: "The Lean Startup  Eric Ries",
       provider: "Amazonas E4",
       price: "20.00",
+      unit: "S/",
       features: ["paperWhite", "new"],
     },
     {
+      id: 2,
       author: "Instaread",
       title: "Summary of The Lean Startup",
       provider: "Amazonas B1",
       price: "10.00",
+      unit: "S/",
       features: ["paperWhite", "used"],
     },
   ]
