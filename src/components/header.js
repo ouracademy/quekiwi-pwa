e4fe67427d26b9676f6113b68abb78bc3f98552d
artmadeit <this.arthur.email@gmail.com>
2019-02-28 12:37:52 -0500

    feat(header): add register button & use gatsby navigation

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types"
 import React from "react"
 import { Box, Anchor, Button, ResponsiveContext, Text } from "grommet"
 import { Grow } from "grommet-icons"
+import { navigate } from "gatsby"
 
 const Header = ({ siteTitle }) => (
   <header>
@@ -21,20 +22,30 @@ const Header = ({ siteTitle }) => (
             icon={<Grow size="large" />}
             label={size !== "small" && <Text size="xlarge">{siteTitle}</Text>}
           />
-
-          <Button href="/login" plain>
-            <Box
-              pad={{ vertical: "small", horizontal: "medium" }}
-              round="xlarge"
-              background="accent-1"
-            >
-              <Text>Ingresar</Text>
-            </Box>
-          </Button>
+          <Actions />
         </Box>
       )}
     </ResponsiveContext.Consumer>
   </header>
+)
+
+const Actions = () => (
+  <Box direction="row" gap="small">
+    <RoundedButton text="Ingresar" href="/login" />
+    <RoundedButton text="Registrate" href="/sign-up" color="light-2" />
+  </Box>
+)
+
+const RoundedButton = ({ text, href, color = "brand" }) => (
+  <Button onClick={() => navigate(href)} plain>
+    <Box
+      pad={{ vertical: "small", horizontal: "medium" }}
+      round="xlarge"
+      background={color}
+    >
+      <Text>{text}</Text>
+    </Box>
+  </Button>
 )
 
 Header.propTypes = {
