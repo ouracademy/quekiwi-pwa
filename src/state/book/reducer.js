4013178c861462d00090f52eb74e255f8e6b530f
artmadeit <this.arthur.email@gmail.com>
2019-03-02 17:06:11 -0500

    refactor: reducer switch to switch :o

--- src/state/book/reducer.js
+++ src/state/book/reducer.js
@@ -1,43 +1,37 @@
-export const reducer = (prevState, action) => {
-  if (action.type === "BOOK_COPIES_REQUESTED") {
-    return {
-      ...prevState,
-    }
-  }
-  if (action.type === "BOOK_COPIES_ADDED") {
-    return {
-      ...prevState,
-      data: [...prevState.data, action.payload],
-    }
-  }
-  if (action.type === "BOOK_COPIES_SAVED") {
-    const index = prevState.data.findIndex(
-      book => (action.payload.id = book.id)
-    )
-    const newData = prevState.data.map((book, i) => {
-      if (index === i) {
-        book = action.payload
+export const reducer = (prevState = { book: {}, data: [] }, action) => {
+  const { type } = action
+
+  switch (type) {
+    case "BOOK_COPIES_REQUESTED":
+      return prevState
+    case "BOOK_COPIES_ADDED":
+      return {
+        ...prevState,
+        data: [...prevState.data, action.payload],
       }
-      return book
-    })
-    return {
-      ...prevState,
-      data: newData,
-    }
-  }
+    case "BOOK_COPIES_SAVED":
+      const newData = prevState.data.map(book => {
+        if (action.payload.id === book.id) {
+          book = action.payload
+        }
+        return book
+      })
 
-  if (action.type === "BOOK_COPIES_DELETED") {
-    return {
-      ...prevState,
-      data: prevState.data.filter(book => book.id !== action.payload),
-    }
+      return {
+        ...prevState,
+        data: newData,
+      }
+    case "BOOK_COPIES_DELETED":
+      return {
+        ...prevState,
+        data: prevState.data.filter(book => book.id !== action.payload),
+      }
+    case "BOOK_ADDED":
+      return {
+        ...prevState,
+        book: action.payload,
+      }
+    default:
+      return prevState
   }
-  if (action.type === "BOOK_ADDED") {
-    return {
-      ...prevState,
-      book: action.payload,
-    }
-  }
-
-  return { book: {}, data: [] }
 }
