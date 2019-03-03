696b5f998625fc0c54c00541fe2a15847032cfd2
qpdian <qpdiam@gmail.com>
2019-03-02 22:11:03 -0500

    feat:  save book copie

--- src/components/book/book-copies.js
+++ src/components/book/book-copies.js
@@ -1,81 +1,13 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { connect } from "react-redux"
 import { Add, Trash, Edit } from "grommet-icons"
 import { Form, FormField, Box, Button } from "grommet"
 import { addBookCopie, saveCopie, deleteCopie } from "../../state/book/actions"
+import { suggestionsBasedCurrentFeatures } from "./feature-suggestions"
 const ReactTags = require("react-tag-autocomplete")
-
-const BookCopie = ({ data, save, remove }) => {
-  const submitCopie = ({ value }) => {
-    console.log(value)
-    save(value)
-  }
-  const deleteCopie = () => {
-    remove(data.id)
-  }
-
-  return (
-    <Box
-      direction="column"
-      pad="medium"
-      round="small"
-      border={{
-        side: "all",
-        color: "border",
-      }}
-    >
-      <Form onSubmit={submitCopie} value={data}>
-        <BookFeatures />
-        <FormField label="Precio" name="price" />
-        <FormField label="Cantidad" name="quantity" />
-        <Box direction="row" justify="end" fill>
-          <Button type="submit" icon={<Edit />} />
-          <Button icon={<Trash />} onClick={deleteCopie} />
-        </Box>
-      </Form>
-    </Box>
-  )
-}
-
-const BookFeatures = () => {
-  const suggestions = [
-    { name: "Usado", type: "time", uniqueInType: true },
-    { name: "Nuevo", type: "time", uniqueInType: true },
-    { name: "Hoja blanca", type: "color", uniqueInType: true },
-    { name: "Hoja bulqui", type: "color", uniqueInType: true },
-    { name: "Tapa dura", type: "any" },
-  ]
-  const [tags, setTags] = useState([])
-  const handleDelete = index => {
-    setTags(tags.filter((tag, i) => i !== index))
-  }
-
-  const handleAddition = ({ name, type = "any" }) => {
-    setTags([...tags, { name, type }])
-  }
-
-  const handleValidate = ({ uniqueInType, type }) => {
-    const alreadyExistType = uniqueInType && tags.find(tag => tag.type === type)
-    return !alreadyExistType
-  }
-  return (
-    <FormField label="Características" name="features">
-      <ReactTags
-        placeholder=""
-        tags={tags}
-        suggestions={suggestions}
-        handleDelete={handleDelete}
-        handleAddition={handleAddition}
-        handleValidate={handleValidate}
-        allowNew={true}
-      />
-    </FormField>
-  )
-}
-
 const BookCopies = ({ data, bookId, addBookCopie, saveCopie, deleteCopie }) => {
   const addCopie = () => {
-    addBookCopie({ bookId })
+    addBookCopie({ bookId, id: data.length + 1 })
   }
   return (
     <Box>
@@ -95,9 +27,76 @@ const BookCopies = ({ data, bookId, addBookCopie, saveCopie, deleteCopie }) => {
     </Box>
   )
 }
+const BookCopie = ({ data = {}, save, remove }) => {
+  const [features, setFeatures] = useState(data.features || [])
+  const [suggestions, setSuggestions] = useState(
+    suggestionsBasedCurrentFeatures(features)
+  )
+  const submitCopie = ({ value }) => {
+    save({ ...data, ...value, features: features })
+  }
+  const deleteCopie = () => {
+    remove(data.id)
+  }
+
+  const changeFeatures = features => {
+    setSuggestions(suggestionsBasedCurrentFeatures(features))
+    setFeatures(features)
+  }
+
+  return (
+    <Box
+      direction="column"
+      pad="medium"
+      round="small"
+      border={{
+        side: "all",
+        color: "border",
+      }}
+    >
+      <Form onSubmit={submitCopie} value={data}>
+        <BookFeatures
+          features={features}
+          changeFeatures={changeFeatures}
+          suggestions={suggestions}
+        />
+        <FormField label="Precio" name="price" />
+        <FormField label="Cantidad" name="quantity" />
+        <Box direction="row" justify="end" fill>
+          <Button type="submit" icon={<Edit />} />
+          <Button icon={<Trash />} onClick={deleteCopie} />
+        </Box>
+      </Form>
+    </Box>
+  )
+}
+
+const BookFeatures = ({ features = [], suggestions = [], changeFeatures }) => {
+  const handleDelete = index => {
+    changeFeatures(features.filter((tag, i) => i !== index))
+  }
+
+  const handleAddition = ({ name, type = "any" }) => {
+    changeFeatures([...features, { name, type }])
+  }
+
+  return (
+    <FormField label="Características" name="features">
+      <ReactTags
+        placeholder=""
+        tags={features}
+        suggestions={suggestions}
+        handleDelete={handleDelete}
+        handleAddition={handleAddition}
+        allowNew={false}
+      />
+    </FormField>
+  )
+}
+
 const mapStateToProps = ({ book }) => ({
   data: book.data,
-  bookId: book.bookId,
+  bookId: book.book.id,
 })
 
 export default connect(
