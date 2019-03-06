a0c090446c40e85d5b3e14b11d08901c3d421dcf
qpdian <qpdiam@gmail.com>
2019-03-06 16:52:23 -0500

    feat: messages for step 1

--- src/pages/book/step-1.js
+++ src/pages/book/step-1.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box } from "grommet"
+import { Box, Text } from "grommet"
 import { Link, navigate } from "@reach/router"
 
 import { SearchInput } from "../../components/search-input"
@@ -21,22 +21,18 @@ export const allBooks = [
 ]
 
 export const Step1 = ({ children }) => {
-  const search = term => navigate(`/book/register/step-1/search/${term}`)
+  const search = term => {
+    navigate(`/book/register/step-1/search/${term}`)
+  }
 
   return (
-    <div>
-      <Box direction="row" justify="between">
-        <h3>1. Busca tu libro</h3>
-        <div>
-          ¿No lo encontraste?{" "}
-          <Link to="/book/register/step-2">Registralo aquí</Link>
-        </div>
-      </Box>
-      <Box>
+    <Box gap="medium" direction="column">
+      <Box direction="column" justify="between">
+        <h4>¿Qué título tiene?</h4>
         <SearchInput suggestionsFor={getSuggestions} onChoose={search} />
-        {children}
       </Box>
-    </div>
+      {children}
+    </Box>
   )
 }
 
@@ -45,7 +41,34 @@ export const ListBooks = ({ term = "" }) => {
 
   const books = allBooks.filter(byTitle(term))
 
-  return <Books books={books} onChooseBook={onChooseBook} />
+  const [existBooks, emptyBooks] = [
+    {
+      message: "¿Cuál de los siguientes es el libro que vas a registrar?",
+      messageForRegister: "¿No lo encontraste?",
+    },
+    {
+      message:
+        "No encontramos libros que correspondan al titulo que has escrito",
+      messageForRegister: "Pero tranquilo",
+    },
+  ]
+
+  return (
+    <Box direction="column">
+      <Box direction="row" justify="between" align="start">
+        <h4>{books.length > 0 ? existBooks.message : emptyBooks.message}</h4>
+        <Box align="start" gap="small" direction="row">
+          <Text color="brand" weight="bold">
+            {books.length > 0
+              ? existBooks.messageForRegister
+              : emptyBooks.messageForRegister}
+          </Text>
+          <Link to="/book/register/step-2">Registralo aquí</Link>
+        </Box>
+      </Box>
+      <Books books={books} onChooseBook={onChooseBook} />
+    </Box>
+  )
 }
 
 const getSuggestions = term => {
