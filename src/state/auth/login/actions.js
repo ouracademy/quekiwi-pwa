b195632eb759a388b6ed9dea5aba249ea6bee446
qpdian <qpdiam@gmail.com>
2019-03-08 16:22:42 -0500

    refactor: rename standard to standardRequest

--- src/state/auth/login/actions.js
+++ src/state/auth/login/actions.js
@@ -1,6 +1,6 @@
-import { getStandardFor } from "../../standard"
+import { getStandardRequestFor } from "../../standard"
 
-const { actionTypes, actionCreators, reducer } = getStandardFor("LOGIN")
+const { actionTypes, actionCreators, reducer } = getStandardRequestFor("LOGIN")
 
 export const [LOGIN_REQUESTED] = actionTypes
 export const [login, loginSuccessFully, loginFailed] = actionCreators
--- src/state/auth/sign-up/actions.js
+++ src/state/auth/sign-up/actions.js
@@ -1,6 +1,6 @@
-import { getStandardFor } from "../../standard"
+import { getStandardRequestFor } from "../../standard"
 
-const { actionTypes, actionCreators, reducer } = getStandardFor("SIGNUP")
+const { actionTypes, actionCreators, reducer } = getStandardRequestFor("SIGNUP")
 
 export const [SIGNUP_REQUESTED] = actionTypes
 export const [signUp, signUpSuccessFully, signUpFailed] = actionCreators
