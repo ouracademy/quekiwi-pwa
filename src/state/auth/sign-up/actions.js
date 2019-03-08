f84d955d1d94f2f4fdf3e2a9c4fd6692b9451b70
artmadeit <this.arthur.email@gmail.com>
2019-03-08 17:06:27 -0500

    refactor(sign-up): requested action type

--- src/state/auth/sign-up/actions.js
+++ src/state/auth/sign-up/actions.js
@@ -2,7 +2,7 @@ import { getStandardRequestFor } from "../../standard"
 
 const { actionTypes, actionCreators, reducer } = getStandardRequestFor("SIGNUP")
 
-export const [SIGNUP_REQUESTED] = actionTypes
+export const [SIGN_UP_REQUESTED] = actionTypes
 export const [signUp, signUpSuccessFully, signUpFailed] = actionCreators
 
 export { reducer }
--- src/state/auth/sign-up/epics.js
+++ src/state/auth/sign-up/epics.js
@@ -2,11 +2,11 @@ import { ofType } from "redux-observable"
 import { ajax } from "rxjs/ajax"
 import { mergeMap, map, catchError } from "rxjs/operators"
 import { of } from "rxjs"
-import { SIGNUP_REQUESTED, signUpFailed, signUpSuccessFully } from "./actions"
+import { SIGN_UP_REQUESTED, signUpFailed, signUpSuccessFully } from "./actions"
 
 export const signUpEpic = action$ =>
   action$.pipe(
-    ofType(SIGNUP_REQUESTED),
+    ofType(SIGN_UP_REQUESTED),
     mergeMap(action =>
       ajax.post("http://localhost:3000/auth/signup", action.payload).pipe(
         map(signUpSuccessFully),
