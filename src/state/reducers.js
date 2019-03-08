eb4a53d515421cb09a6cdd0422e8e9b30d57678c
qpdian <qpdiam@gmail.com>
2019-03-08 16:23:02 -0500

    feat: update epics and reducer

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux"
 import { auth } from "./auth/reducer"
-import { reducer as book } from "./book/reducer"
+import { books as book } from "./book/reducer"
 
 export const rootReducer = combineReducers({ auth, book })
