a4475b0a88998696b1f3f2e8f289a9b8e9a63e58
artmadeit <this.arthur.email@gmail.com>
2019-03-13 16:05:00 -0500

    refactor(auth): use standard epic

--- src/state/auth/login/epics.js
+++ src/state/auth/login/epics.js
@@ -1,16 +1,8 @@
-import { ofType } from "redux-observable"
-import { ajax } from "rxjs/ajax"
-import { mergeMap, map, catchError } from "rxjs/operators"
-import { of } from "rxjs"
 import { LOGIN_REQUESTED, loginFailed, loginSuccessFully } from "./actions"
+import { standardEpic } from "../../standard-request/epic"
 
-export const loginEpic = action$ =>
-  action$.pipe(
-    ofType(LOGIN_REQUESTED),
-    mergeMap(action =>
-      ajax.post("http://localhost:3000/auth/login", action.payload).pipe(
-        map(loginSuccessFully),
-        catchError(error => of(loginFailed(error)))
-      )
-    )
-  )
+export const loginEpic = standardEpic(
+  LOGIN_REQUESTED,
+  loginSuccessFully,
+  loginFailed
+)("http://localhost:3000/auth/login")
--- src/state/auth/sign-up/epics.js
+++ src/state/auth/sign-up/epics.js
@@ -1,16 +1,8 @@
-import { ofType } from "redux-observable"
-import { ajax } from "rxjs/ajax"
-import { mergeMap, map, catchError } from "rxjs/operators"
-import { of } from "rxjs"
 import { SIGN_UP_REQUESTED, signUpFailed, signUpSuccessFully } from "./actions"
+import { standardEpic } from "../../standard-request/epic"
 
-export const signUpEpic = action$ =>
-  action$.pipe(
-    ofType(SIGN_UP_REQUESTED),
-    mergeMap(action =>
-      ajax.post("http://localhost:3000/auth/signup", action.payload).pipe(
-        map(signUpSuccessFully),
-        catchError(error => of(signUpFailed(error)))
-      )
-    )
-  )
+export const signUpEpic = standardEpic(
+  SIGN_UP_REQUESTED,
+  signUpSuccessFully,
+  signUpFailed
+)("http://localhost:3000/auth/signup")
