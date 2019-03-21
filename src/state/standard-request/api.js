a4217a8428027014c4cc63b5b8f7c8caa426386c
artmadeit <this.arthur.email@gmail.com>
2019-03-21 12:16:04 -0500

    fix(api): not sending  json content

--- src/state/standard-request/api.js
+++ src/state/standard-request/api.js
@@ -3,14 +3,23 @@ import { authHeaders, authToken } from "./auth"
 
 const defaultGetHeaders = () => undefined
 
-// create an http REST client with base headers
-const apiFrom = (baseUrl, getHeaders = defaultGetHeaders) => ({
-  get: url => query => ajax.get(`${baseUrl}/${url}`, getHeaders()),
-  post: url => body => ajax.post(`${baseUrl}/${url}`, body, getHeaders()),
-  patch: url => body => ajax.patch(`${baseUrl}/${url}`, body, getHeaders()),
-  delete: url => ({ id }) =>
-    ajax.delete(`${baseUrl}/${url}/${id}`, getHeaders()),
+const withJSONContent = getHeaders => () => ({
+  "Content-Type": "application/json",
+  ...getHeaders(),
 })
+
+// create an http REST client with base headers
+const apiFrom = (baseUrl, getHeaders = defaultGetHeaders) => {
+  const headers = withJSONContent(getHeaders)
+
+  return {
+    get: url => query => ajax.get(`${baseUrl}/${url}`, headers()),
+    post: url => body => ajax.post(`${baseUrl}/${url}`, body, headers()),
+    patch: url => body => ajax.patch(`${baseUrl}/${url}`, body, headers()),
+    delete: url => ({ id }) =>
+      ajax.delete(`${baseUrl}/${url}/${id}`, headers()),
+  }
+}
 
 const baseUrl = "http://localhost:3000"
 export const api = apiFrom(baseUrl)
